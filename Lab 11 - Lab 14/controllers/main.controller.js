fb2a6b2997f80295a58ed88809de606d733c1179
Rommel <rommtoledo@gmail.com>
2024-03-11 17:56:51 -0600

    Correct the link to the doc with the answers

--- Lab 11 - Lab 14/controllers/main.controller.js
+++ Lab 11 - Lab 14/controllers/main.controller.js
@@ -6,7 +6,7 @@ exports.get_root = (request, response, next) => {
         pagePrimaryTitle: "Testing How To Handle Routes",
         includeContent: true,
         content: `
-        <a href="https://docs.google.com/document/d/1201l2ge3rhdCpdNmBi7jHMV8zsxhtWfLNue8585kfr8/edit#heading=h.ptzwajrrd986">Link To Document with the Question's Answer</a>
+        <a href="https://docs.google.com/document/d/1201l2ge3rhdCpdNmBi7jHMV8zsxhtWfLNue8585kfr8/edit#heading=h.ez6jpdwpkben">Link To Document with the Question's Answer</a>
         `,
     });
 };
