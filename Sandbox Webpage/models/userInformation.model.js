59b1657132ecf80f481c23de88d75bcaa366a136
Rommel <rommtoledo@gmail.com>
2024-04-08 00:13:08 -0600

    Add the procedure to change a user role

--- Sandbox Webpage/models/userInformation.model.js
+++ Sandbox Webpage/models/userInformation.model.js
@@ -19,7 +19,7 @@ module.exports = class User {
             return db.execute('call createNewUser(?, ?, ?, ?, ?, ?)', [this.username, this.name, encrypted_password, this.email, this.age, this.gender])
         })
         .catch((error) => {
-            console.log(error);
+            console.error('Error creating new user:', error);
         })
     }
 
@@ -54,6 +54,26 @@ module.exports = class User {
             join privilege pr on h.idprivilege = pr.id
             where u.username = ?
         `, [username]);
+    }
+
+    // TODO: Add the implementation of the following code in the admin controls view
+    // As of now, the previous roles doesn't get store in the database but rather overwrite
+    static changeUserRole(username, role){
+        const roles = {
+            Member: 1,
+            'Premium Member': 2,
+            Admin: 3
+        };
+
+        const roleId = roles[role];
+
+        return db.execute('call changeUserRole(?, ?)', [username, roleId])
+        .then(result => {
+            console.log('User role updated successfully');
+        })
+        .catch((error) => {
+            console.error('Error updating user role:', error);
+        });
     }
 
 }
