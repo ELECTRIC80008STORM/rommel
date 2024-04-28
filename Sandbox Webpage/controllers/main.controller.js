a1df2b1901b7efa478f2ba2ca04a22a91e47dbe6
Rommel <rommtoledo@gmail.com>
2024-04-28 17:47:05 -0600

    Use Cat API to show a grid of cute cats images

--- Sandbox Webpage/controllers/main.controller.js
+++ Sandbox Webpage/controllers/main.controller.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch'); // Simplifies the use of APIs
+
 exports.get_uploadImage = (request, response, next) => {
     response.render('uploadImage', {
         csrfToken: request.csrfToken(),
@@ -26,16 +28,30 @@ exports.post_uploadImage = (request, response, next) => {
 };
 
 exports.get_root = (request, response, next) => {
-    // console.log(request.session.user);
-    response.render('article', {
-        isUserRegistered: (typeof request.session.user !== 'undefined')? true : false,
-        userProfileView: (typeof request.session.user !== 'undefined')? '/user/profile' : '/user/signup',
-        username: (typeof request.session.user !== 'undefined')? request.session.user.username : '',
-        pagePrimaryTitle: "Testing How To Handle Routes",
-        includeContent: true,
-        content: `
-        <a href="https://docs.google.com/document/d/1201l2ge3rhdCpdNmBi7jHMV8zsxhtWfLNue8585kfr8">Link To Document with the Question's Answer</a>
-        `,
-        privileges: request.session.privileges || [],
-    });
+    
+    const url = 'https://api.thecatapi.com/v1/images/search?limit=9';
+    const api_key = 'live_AfdDv0ksJCKzy7VRrJL2fvNvgSH2nwUUgzZijWZAUtjWMaWNhfBGpx6BrVrEQKnn';
+
+    fetch(url, { headers: { 'x-api-key': api_key } })
+    .then((response) => response.json())
+    .then((cats) => {
+        console.log(cats);
+        response.render('article', {
+            isUserRegistered: (typeof request.session.user !== 'undefined')? true : false,
+            userProfileView: (typeof request.session.user !== 'undefined')? '/user/profile' : '/user/signup',
+            username: (typeof request.session.user !== 'undefined')? request.session.user.username : '',
+            pagePrimaryTitle: "Testing How To Handle Routes",
+            includeContent: true,
+            content: `
+            <a href="https://docs.google.com/document/d/1201l2ge3rhdCpdNmBi7jHMV8zsxhtWfLNue8585kfr8">Link To Document with the Question's Answer</a>
+            `,
+            privileges: request.session.privileges || [],
+            catImagesShown: true,
+            cats: cats
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching cat images:', error);
+        response.status(500).send('Unable to load cat images');
+    })
 };
